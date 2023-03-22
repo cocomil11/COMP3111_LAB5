d8ae1ed6581cb7ce3be45db6b4496898a589ae8e
Kentaro Takahira <kentaro.takahira@gmail.com>
2023-03-22 21:13:49 +0800

    [modify] testcases

--- src/main/Library.java
+++ src/main/Library.java
@@ -1,27 +1,24 @@
 package main;
+import java.util.Arrays;
 
 public class Library {
 	public static void main(String arg[]) {
 		System.out.println("Hello, JUnit 4!");	
 	}
 	
-	public static int computeOne() {
-		return 1;
-	}
+
 	
-	public static boolean isItTrue() {
-		return true;
+	public static int[] mySort(int[] inputArray) {
+		int[] sorted = inputArray;
+		Arrays.sort(sorted);
+		return sorted;
 	}
-	
-	public static boolean isEvenNumber(int num) {
-		if ((num % 2) == 0)
+	public static boolean detectOdd(int inputNumber) {
+		if (inputNumber%2 == 1) {
 			return true;
-		else
+		}else {
 			return false;
-	}
-	
-	public static int[] sort(int[] anyArray) {
-		return new int[] {1, 2, 3, 4, 5};
+		} 
 	}
 }
 
--- src/main/LibraryTester.java
+++ src/main/LibraryTester.java
@@ -5,38 +5,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class LibraryTester {
-	
-	int[] arrAscending, arrDescending;
-	
-	@Before
-	public void setUp() throws Exception {
-		arrAscending = new int[] {1, 2, 3, 4, 5};
-		arrDescending = new int[] {5, 4, 3, 2, 1};
+
+	@Test // Annotation. The following function is treated as test. 
+	public void mySort() {
+		int[] input = new int[] {2, 3, 1, 4, 5};
+		int[] expected = new int[] {1, 2, 3, 4, 5};
+		int[] actual = Library.mySort(input);
+		assertArrayEquals(expected, actual);
 	}
 
 	@Test
-	public void computeOneWithValidInput() {
-		assertEquals(1, Library.computeOne());
+	public void detectEven1() {
+		
+		int input1 = 1; 
+		boolean expect1 = true;
+		boolean actual1 = Library.detectOdd(input1);
+
+		assertEquals(expect1, actual1);
+		
 	}
 	
 	@Test
-	public void isItTrueWithValidInput() {
-		assertTrue(Library.isItTrue());
-	}
-	
-	@Test
-	public void sortDescendingOrder() {
-		assertArrayEquals(arrAscending, Library.sort(arrDescending));
-	}
-	
-	@Test
-	public void sortAscendingOrder() {
-		assertArrayEquals(arrAscending, Library.sort(arrAscending));
+	public void detectEven2() {
+		
+		int input2 = 2; 
+		boolean expect2 = true;
+		boolean actual2 = Library.detectOdd(input2);
+
+		assertEquals(expect2, actual2);
+		
 	}
 
-	@Test
-	public void isEvenNumReturnTrue() {
-		assertTrue(Library.isEvenNumber(2));
-	}	
 	
 }
